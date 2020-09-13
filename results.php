f393bf54804fa02ec1a901bb2caa2dca7510ebb7
Luke-Sheltraw <lukesheltraw@gmail.com>
2020-09-13 10:49:20 -0700

    Create results.php

---
+++ results.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Speed and Dexterity | Speed Test</title>
+<link rel="stylesheet" href="main.css" />
+</head>
+<body>
+<?php
+	echo $_POST['logdata'];
+?>
+</body>
+</html>
